60ee62141de57c8f279278586ac55d959d81a199
shupenggui <sgui2@ur.rochester.edu>
2019-10-18 16:31:21 -0400

    clean utee

--- utee/kmeans.py
+++ utee/kmeans.py
@@ -1,5 +1,7 @@
 '''
+Modified based on
 https://github.com/overshiki/kmeans_pytorch
+Thanks to the original repo
 '''
 import torch
 import numpy as np 
@@ -14,7 +16,6 @@ def choose_centers(X, centers):
 		choice_cluster: tensor of shape (N,). idx of closest center.
 	'''
 	with torch.no_grad():
-		# X, centers = X.cuda(), centers.cuda()
 		assert len(list(X.size())) == 2 and len(list(centers.size())) == 2, "{}-{}".format(X.shape, centers.shape)
 
 		N, d1 = X.shape
@@ -27,14 +28,6 @@ def choose_centers(X, centers):
 		choice_cluster = torch.argmin(distance, dim=1)
 		torch.cuda.empty_cache()
 
-		# distance_min = torch.from_numpy(np.full(shape=N, fill_value=np.Inf)).float().cuda() # tensor of shape (N,). distance to the closest center.
-		# choice_cluster = torch.from_numpy(np.full(shape=N, fill_value=np.NaN)).float().cuda()
-		# for i in range(K):
-		# 	distance = ((X - centers[i,:].repeat(N,1))**2.0).sum(dim=1).squeeze()
-		# 	idx = torch.le(distance, distance_min).nonzero()
-		# 	distance_min[idx] = distance[idx]
-		# 	choice_cluster[idx] = i
-		# 	torch.cuda.empty_cache()
 
 	return choice_cluster
 
@@ -46,7 +39,6 @@ def forgy_initialization(X, K):
 		K: number of clusters
 	'''
 	with torch.no_grad():
-		# N = list(X.size())[0]
 		N = X.shape[0]
 		indices = torch.randperm(N)[0:K]
 		initial_centers = X[indices]
@@ -61,7 +53,6 @@ def lloyd(X, K, tol=1e-4, max_iter=20, verbose=False, iscuda=True):
 		K: number of clusters
 	'''
 	with torch.no_grad():
-		# X = X.cuda()
 
 		# initalize centers:
 		centers = forgy_initialization(X, K)
@@ -73,29 +64,17 @@ def lloyd(X, K, tol=1e-4, max_iter=20, verbose=False, iscuda=True):
 
 		for i in range(max_iter):
 			# M step:
-			# print(X.shape)
 			choice_cluster = choose_centers(X, centers)
 			
 			centers_old = centers.clone()
 
 			one_hot = one_hot.zero_()
-			# print(one_hot.shape)
-			# print(choice_cluster.unsqueeze(1).type(torch.cuda.LongTensor).shape)
 			one_hot.scatter_(1, choice_cluster.unsqueeze(1), 1)
 			Xp = (X * one_hot).sum(dim=0)
 			Xsum = one_hot.sum(dim=0)
 			idx = torch.nonzero(Xsum)
 			Xsum[idx] = 1./Xsum[idx]
 			centers = (Xp * Xsum).unsqueeze(1)
-			# centers = Xp.mean(dim=0).unsqueeze(1)
-			# print(X.shape)
-			# print(centers.shape)
-
-			# E step:
-			# for j in range(K):
-			# 	idx = torch.nonzero(choice_cluster==j).squeeze()
-			# 	selected = torch.index_select(X, 0, idx)
-			# 	centers[j] = selected.mean(dim=0)
 			
 			# breaking condition:
 			center_shift = torch.sum(torch.sqrt(torch.sum((centers - centers_old) ** 2, dim=1)))
@@ -115,17 +94,12 @@ def forgy_initialization_nnz(X, Xp, K):
 		K: number of clusters
 	'''
 	with torch.no_grad():
-		# N = list(X.size())[0]
 		nnz_dix = torch.nonzero(Xp.view(-1))
 		nnz_dix.cuda()
-		# print("nnz idx shape {}".format(nnz_dix.shape))
 		indices = torch.randperm(len(nnz_dix))[0:K-1]
 		zero_el = torch.zeros(1, 1)
 		zero_el = zero_el.cuda()
 		idx0 = nnz_dix[indices].squeeze()
-		# print("X shape {}".format(X.shape))
-		# print("idx0 shape {}".format(idx0.shape))
-		# print("X idx shape {}".format(X[idx0].shape))
 		initial_centers = torch.cat([X[idx0], zero_el])
 	return initial_centers
 
@@ -150,29 +124,17 @@ def lloyd_nnz(X, Xp, K, tol=1e-4, max_iter=20, verbose=False, iscuda=True):
 
 		for i in range(max_iter):
 			# M step:
-			# print(X.shape)
 			choice_cluster = choose_centers(X, centers)
 			
 			centers_old = centers.clone()
 
 			one_hot = one_hot.zero_()
-			# print(one_hot.shape)
-			# print(choice_cluster.unsqueeze(1).type(torch.cuda.LongTensor).shape)
 			one_hot.scatter_(1, choice_cluster.unsqueeze(1), 1)
 			Xp = (X * one_hot).sum(dim=0)
 			Xsum = one_hot.sum(dim=0)
 			idx = torch.nonzero(Xsum)
 			Xsum[idx] = 1./Xsum[idx]
 			centers = (Xp * Xsum).unsqueeze(1)
-			# centers = Xp.mean(dim=0).unsqueeze(1)
-			# print(X.shape)
-			# print(centers.shape)
-
-			# E step:
-			# for j in range(K):
-			# 	idx = torch.nonzero(choice_cluster==j).squeeze()
-			# 	selected = torch.index_select(X, 0, idx)
-			# 	centers[j] = selected.mean(dim=0)
 			
 			# breaking condition:
 			center_shift = torch.sum(torch.sqrt(torch.sum((centers - centers_old) ** 2, dim=1)))
@@ -191,11 +153,9 @@ def lloyd_nnz_fixed_0_center(X, Xp, K, tol=1e-4, max_iter=20, verbose=False, isc
 		K: number of clusters
 	'''
 	with torch.no_grad():
-		# X = X.cuda()
 
 		# initalize centers:
 		centers = forgy_initialization_nnz(X, Xp, K+1)
-		# print(centers.shape)
 		one_hot = torch.zeros(X.shape[0], K + 1)
 		ones = torch.ones(K)
 		if iscuda:
@@ -204,29 +164,18 @@ def lloyd_nnz_fixed_0_center(X, Xp, K, tol=1e-4, max_iter=20, verbose=False, isc
 
 		for i in range(max_iter):
 			# M step:
-			# print(X.shape)
 			choice_cluster = choose_centers(X, centers)
 			
 			centers_old = centers.clone()
 
 			one_hot = one_hot.zero_()
-			# print(one_hot.shape)
-			# print(choice_cluster.unsqueeze(1).type(torch.cuda.LongTensor).shape)
+
 			one_hot.scatter_(1, choice_cluster.unsqueeze(1), 1)
 			Xp = (X * one_hot).sum(dim=0)
 			Xsum = one_hot.sum(dim=0)
 			idx = torch.nonzero(Xsum)
 			Xsum[idx] = 1./Xsum[idx]
 			centers[:K] = (Xp * Xsum).unsqueeze(1)[:K]
-			# centers = Xp.mean(dim=0).unsqueeze(1)
-			# print(X.shape)
-			# print(centers.shape)
-
-			# E step:
-			# for j in range(K):
-			# 	idx = torch.nonzero(choice_cluster==j).squeeze()
-			# 	selected = torch.index_select(X, 0, idx)
-			# 	centers[j] = selected.mean(dim=0)
 			
 			# breaking condition:
 			center_shift = torch.sum(torch.sqrt(torch.sum((centers - centers_old) ** 2, dim=1)))
@@ -237,28 +186,6 @@ def lloyd_nnz_fixed_0_center(X, Xp, K, tol=1e-4, max_iter=20, verbose=False, isc
 
 	return choice_cluster, centers
 
-# def forgy_initialization_nnz(X, Xp, K):
-# 	'''
-# 	Forgy initialization algorithm for kmeans: randomly select K out of N samples to be the initial centers.
-# 	Args:
-# 		X: tensor of size (N, d). data points.
-# 		K: number of clusters
-# 	'''
-# 	with torch.no_grad():
-# 		# N = list(X.size())[0]
-# 		nnz_dix = torch.nonzero(Xp.view(-1))
-# 		nnz_dix.cuda()
-# 		# print("nnz idx shape {}".format(nnz_dix.shape))
-# 		indices = torch.randperm(len(nnz_dix))[0:K-1]
-# 		zero_el = torch.zeros(1, 1)
-# 		zero_el = zero_el.cuda()
-# 		idx0 = nnz_dix[indices].squeeze()
-# 		# print("X shape {}".format(X.shape))
-# 		# print("idx0 shape {}".format(idx0.shape))
-# 		# print("X idx shape {}".format(X[idx0].shape))
-# 		initial_centers = torch.cat([X[idx0], zero_el])
-# 	return initial_centers
-
 
 def forgy_initialization_fixed_nnz(X, Xp, K):
 	'''
@@ -268,7 +195,6 @@ def forgy_initialization_fixed_nnz(X, Xp, K):
 		K: number of clusters
 	'''
 	with torch.no_grad():
-		# N = list(X.size())[0]
 		nnz_dix = torch.nonzero(Xp.view(-1))
 		nnz_dix.cuda()
 		indices = torch.randperm(len(nnz_dix))[0:K-1]
@@ -299,29 +225,18 @@ def lloyd_fixed_nnz(X, Xp, K, tol=1e-4, max_iter=20, verbose=False, iscuda=True)
 		Xnnz = X[nnz_idx].squeeze(-1)
 		for i in range(max_iter):
 			# M step:
-			# print(X.shape, nnz_idx.shape, Xnnz.shape)
 			choice_cluster = choose_centers(Xnnz, centers)
 			
 			centers_old = centers.clone()
 
 			one_hot = one_hot.zero_()
-			# print(one_hot.shape)
-			# print(choice_cluster.unsqueeze(1).type(torch.cuda.LongTensor).shape)
 			one_hot.scatter_(1, choice_cluster.unsqueeze(1), 1)
 			Xp = (Xnnz * one_hot).sum(dim=0)
 			Xsum = one_hot.sum(dim=0)
 			idx = torch.nonzero(Xsum)
 			Xsum[idx] = 1./Xsum[idx]
 			centers = (Xp * Xsum).unsqueeze(1)
-			# centers = Xp.mean(dim=0).unsqueeze(1)
-			# print(X.shape)
-			# print(centers.shape)
-
 			# E step:
-			# for j in range(K):
-			# 	idx = torch.nonzero(choice_cluster==j).squeeze()
-			# 	selected = torch.index_select(X, 0, idx)
-			# 	centers[j] = selected.mean(dim=0)
 			
 			# breaking condition:
 			center_shift = torch.sum(torch.sqrt(torch.sum((centers - centers_old) ** 2, dim=1)))
--- utee/quantize.py
+++ utee/quantize.py
@@ -6,7 +6,6 @@ from kmeans import lloyd, lloyd_nnz, lloyd_fixed_nnz, lloyd_nnz_fixed_0_center
 
 def print_params(model):
 	for i, (param_name, W) in enumerate(model.named_parameters()):
-		# print(param_name, W.size(), type(W))
 		if i == 0:
 			print(W[0,0,:,:].data.cpu().numpy())
 
@@ -32,7 +31,6 @@ def quantize_kmeans(model, bit_depth=8, quant_bias=False, verbose=False):
 	'''
 	K = int(2**bit_depth) # cluster number in kmeans
 	for param_name, W in model.named_parameters():
-		# if W.dim()==4 or (W.dim()==2 and 'linear' in param_name): # Only quant weight in conv, FC layers.
 		if param_name.strip().split(".")[-1] in ["weight", "weightA", "weightB", "weightC"] and param_name.strip().split(".")[-2][:2] != "bn" and W.dim() > 1:
 			if verbose:
 				print(param_name, W.size(), type(W))
@@ -46,7 +44,6 @@ def quantize_kmeans(model, bit_depth=8, quant_bias=False, verbose=False):
 				print('centers:', centers.size())
 
 			W.data.copy_(centers[choice_cluster.type(torch.LongTensor)].view(W.size()).data)
-			# print("{}: {}".format(param_name, W.data.unique().shape[0]))
 
 def quantize_kmeans_nnz(model, modelp, bit_depth=8, quant_bias=False, verbose=False):
 	'''
@@ -110,26 +107,12 @@ def quantize_kmeans_fixed_nnz(model, modelp, bit_depth=8, quant_bias=False, verb
 			Wp = sparse_dict[param_name]
 			if len(torch.nonzero(Wp)) <= K-1:
 				continue
-			# print("W and Wp", W.shape, Wp.shape)
 			choice_cluster, centers = lloyd_fixed_nnz(W.view(-1).unsqueeze(-1), Wp.view(-1).unsqueeze(-1), K)
 
-			# print(choice_cluster)
-
 			nnz_idx = torch.nonzero(Wp.view(-1))
-
-			# print(nnz_idx)
-			# print(W.view(-1)[nnz_idx].shape, W.shape, param_name)
-
 			cpW = W.clone().view(-1)
 			oldW = W.clone()
-			# print(cpW.shape)
 			centers_data = centers[choice_cluster.type(torch.LongTensor)].data
-			# print("centers_data", centers_data.shape)
-			# cpW[nnz_idx].data.copy_(centers_data)
-			# print(cpW.shape)
-			# exit(0)
 
 			cpW[nnz_idx] = centers_data
 			W.data.copy_(cpW.view(W.shape).data)
-			# print(torch.norm(oldW - W))
-			# W.view(-1)[nnz_idx].data.copy_(centers[choice_cluster.type(torch.LongTensor)].view(W[nnz_idx].shape).data)
