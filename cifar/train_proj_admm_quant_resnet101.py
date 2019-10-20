5a419bb66c89eeda99d6c82d874c6e794e323f72
shupenggui <sgui2@ur.rochester.edu>
2019-10-20 15:31:28 -0400

    remove redundant code

--- cifar/train_proj_admm_quant_resnet101.py
+++ cifar/train_proj_admm_quant_resnet101.py
@@ -23,9 +23,7 @@ from quantize import quantize_kmeans_nnz_fixed_0_center as kmeans_nnz_fixed_0_ce
 # np.set_printoptions(threshold=np.nan)
 
 import dataset
-# from caffelenet.caffelenet_dense import CaffeLeNet as CLdense
-# from caffelenet.caffelenet_abcv2 import CaffeLeNet as CLabcv2
-# from caffelenet.caffelenet_lr import CaffeLeNet as CLlr
+
 from resnet.resnet_dense import ResNet101 as CLdense
 from resnet.resnet_abcv2 import ResNet101 as CLabcv2
 from resnet.resnet_lr import ResNet101 as CLlr
@@ -52,7 +50,6 @@ parser.add_argument('--defend_algo', default=None, help='adversarial algo for de
 parser.add_argument('--defend_eps', type=float, default=None, help='perturbation radius for defend phase')
 parser.add_argument('--defend_iter', type=int, default=7, help="defend iteration for the adversarial sample computation")
 
-# parser.add_argument('--raw_train', type=bool, default=False, help="raw training without pre-train model loading")
 parser.add_argument("--raw_train", action="store_true")
 parser.add_argument("--abc_special", action="store_true")
 parser.add_argument("--abc_initialize", action="store_true")
@@ -102,7 +99,6 @@ if args.cuda:
 train_loader, test_loader = dataset.get10(batch_size=args.batch_size, data_root=args.data_root, num_workers=1)
 
 algo = {'fgsm': fgsm_gt, 'bim': ifgsm_gt, 'pgd': pgd_gt}
-# attack_algo = algo[args.attack_algo]
 
 attack_algo = algo[args.attack_algo] if args.attack_algo is not None else None
 defend_algo = algo[args.defend_algo] if args.defend_algo is not None else None
@@ -174,16 +170,13 @@ elif args.lr_special:
         weights_list, ranks_up = model_base.svd_global_lowrank_weights(k=args.prune_ratio)
         print(ranks_up)
         model = CLlr(ranks_up)
-        # print([weight.shape for weight in weights_list])
-        # print(weights_list)
+
         model.set_weights(weights_list)
         model.load_state_dict(model_base.state_dict(), strict=False)
-        # descripter = "{}_proj_{}_nnz_{}_quant_{}_bits_{}_".format(defend_name, prune_name, args.prune_ratio, quantize_name, args.quantize_bits)
-        # with open(os.path.join(args.savedir, "param_" + descripter + "lr.npy"), "wb") as filestream:
+
         with open(os.path.join(args.savedir, args.save_model_name[0:-4] + ".npy"), "wb") as filestream:
             pickle.dump(ranks_up, filestream, protocol=pickle.HIGHEST_PROTOCOL)
     else:
-        # with open(os.path.join(args.loaddir, "param_" + descripter + "lr.npy"), "rb") as filestream:
         fname = os.path.join(args.loaddir, 'cifar_alr_' + str(args.prune_ratio) + ".npy")
         print('+++++++++++++++++++')
         print(fname)
@@ -204,10 +197,6 @@ modelu.empty_all()
 
 modelz.load_state_dict(model.state_dict())
 
-# if not args.raw_train:
-#     modelz.load_state_dict()
-# model_feature = torch.nn.DataParallel(model_feature, device_ids=range(args.ngpu))
-
 if args.cuda:
     model = torch.nn.DataParallel(model, device_ids=range(args.ngpu))
     modelu = torch.nn.DataParallel(modelu, device_ids=range(args.ngpu))
@@ -216,10 +205,7 @@ if args.cuda:
     modelu.cuda()
     modelz.cuda()
 
-# optimizer = optim.Adam(model.parameters(), lr=args.lr)
 optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.wd, momentum=0.9)
-# optim_pred = optim.Adam(list(model_feature.parameters()) + list(model_pred.parameters()), lr=args.lr)
-# optim_distortion = optim.Adam(model_distortion.parameters())
 
 decreasing_lr = list(map(int, args.decreasing_lr.split(',')))
 
@@ -237,21 +223,16 @@ try:
             iscuda=args.cuda, adv_iter=3, criterion=F.cross_entropy)
     
     layers = layers_nnz(model, param_name=weight_name)[1]
-    # misc.print_dict(layers, name="NNZ PER LAYER")
 
     layers_n = pt.layers_n(model_base, param_name=["weight"])[1]
     all_num = sum(layers_n.values())
-    # print(all_num)
     sparse_factor = int(all_num * args.prune_ratio)
-    # print(sparse_factor)
     model_size = sparse_factor * args.quantize_bits
     print("\t MODEL SIZE {}".format(model_size))
     weight_bits = [args.quantize_bits for _ in layers] if args.quantize_algo == "model_size_quant" else None
     print("\t weight bits {}".format(weight_bits))
     layernnz = lambda m: list(pt.layers_nnz(m, param_name=weight_name)[1].values())
     param_list = lambda m: pt.param_list(m, param_name=weight_name)
-
-    # print("\t MODEL SIZE {}".format(model_size))
 
     print(args.prune_algo)
 
@@ -281,14 +262,12 @@ try:
     else:
         quantize_lambda = None
 
-    # descripter = "{}_proj_{}_nnz_{}_quant_{}_bits_{}_".format(defend_name, prune_name, args.prune_ratio, quantize_name, args.quantize_bits)
     descripter = ''
     for epoch in range(args.epochs):
 
         if epoch in decreasing_lr:
             optimizer.param_groups[0]['lr'] *= 0.1
         
-        # print("quant_interval {}".format(args.quant_interval))
         message = model_train_proj_prune_admm_quant([model, modelz, modelu], epoch, train_loader, optimizer,
             dfn_algo=defend_algo, dfn_eps=args.defend_eps, 
             log_interval=args.log_interval, iscuda=args.cuda, nnzfix=args.prune_algo=="baseline",
@@ -313,7 +292,6 @@ try:
         misc.model_saver(model, args.savedir, args.save_model_name, "sparse_latest_" + descripter)
         if quantize_algo is not None:
             misc.model_saver(modelz, args.savedir, args.save_model_name, "quant_latest_" + descripter)
-            # best_train_acc = train_acc
 
         if epoch % args.test_interval == 0:
 
@@ -321,9 +299,7 @@ try:
                     atk_algo=attack_algo, atk_eps=args.attack_eps, 
                     iscuda=args.cuda, adv_iter=args.defend_iter, criterion=F.cross_entropy)
 
-            # acc = model_uai_test(model_feature, model_pred, epoch, test_loader, attack_algo, args.attack_eps, iscuda=args.cuda, adv_iter=16, criterion=F.cross_entropy)
             layers = layers_nnz(model, param_name=weight_name)[1]
-            # misc.print_dict(layers, name="MODEL SIZE")
             if quantize_lambda is not None:
                 if isinstance(modelz, nn.DataParallel):
                     modelz.module.replace_bias(model.module, weight_name)
@@ -334,7 +310,6 @@ try:
                         iscuda=args.cuda, adv_iter=args.defend_iter, criterion=F.cross_entropy)
 
                 layers = layers_unique(modelz, normalized=False, param_name=weight_name)[1]
-                # misc.print_dict(layers, name="UNIQUE SIZE")
             if quantize_lambda is not None:
                 dist, dist_layer, relative_layer = util_trts.model_distance(model, modelz, weight_name=weight_name)
                 print("\t model dist: {}, layer-wise dist :{}, relative: {}".format(dist, dist_layer, relative_layer))
@@ -345,23 +320,11 @@ try:
                     print("\t\t closest model saved")
                 
             if acc_adv > best_acc:
-                # new_file = os.path.join(args.loaddir, 'best-{}.pth'.format(epoch))
-                # descripter = args.defend_algo if args.defend_algo is not None else ""
                 misc.model_saver(model, args.savedir, args.save_model_name, "sparse_" + descripter)
                 if quantize_algo is not None:
                     misc.model_saver(modelz, args.savedir, args.save_model_name, "quant_" + descripter)
-                # if args.save_model_name is None:
-                #     if args.defend_algo is not None:
-                #         misc.model_snapshot(model, os.path.join(args.savedir, "sparse_" + args.defend_algo+'_densepretrain.pth'))
-                #         misc.model_snapshot(modelz, os.path.join(args.savedir, "quant_" + args.defend_algo + "_densepretrain.pth"))
-                #     else:
-                #         misc.model_snapshot(model, os.path.join(args.savedir, 'sparse_densepretrain.pth'))
-                #         misc.model_snapshot(modelz, os.path.join(args.savedir, 'quant_densepretrain.pth'))
-                # else:
-                #     misc.model_snapshot(model, os.path.join(args.savedir, "sparse_" + args.save_model_name))
-                #     misc.model_snapshot(modelz, os.path.join(args.savedir, "quant_" + args.save_model_name))
+
                 best_acc = acc_adv
-                # old_file = new_file
 
 except Exception as e:
     import traceback
--- svhn/train_proj_admm_quant.py
+++ svhn/train_proj_admm_quant.py
@@ -23,12 +23,7 @@ from quantize import quantize_kmeans_nnz_fixed_0_center as kmeans_nnz_fixed_0_ce
 # np.set_printoptions(threshold=np.nan)
 
 import dataset
-# from caffelenet.caffelenet_dense import CaffeLeNet as CLdense
-# from caffelenet.caffelenet_abcv2 import CaffeLeNet as CLabcv2
-# from caffelenet.caffelenet_lr import CaffeLeNet as CLlr
-# from resnet.resnet_dense import ResNet34 as CLdense
-# from resnet.resnet_abcv2 import ResNet34 as CLabcv2
-# from resnet.resnet_lr import ResNet34 as CLlr
+
 from wrn.wideresnet_dense import WideResNet as CLdense
 from wrn.wideresnet_abcv2 import WideResNet as CLabcv2
 from wrn.wideresnet_lr import WideResNet as CLlr
@@ -53,7 +48,6 @@ parser.add_argument('--defend_algo', default=None, help='adversarial algo for de
 parser.add_argument('--defend_eps', type=float, default=None, help='perturbation radius for defend phase')
 parser.add_argument('--defend_iter', type=int, default=16, help="defend iteration for the adversarial sample computation")
 
-# parser.add_argument('--raw_train', type=bool, default=False, help="raw training without pre-train model loading")
 parser.add_argument("--raw_train", action="store_true")
 parser.add_argument("--abc_special", action="store_true")
 parser.add_argument("--abc_initialize", action="store_true")
@@ -103,7 +97,6 @@ if args.cuda:
 train_loader, test_loader = dataset.get(batch_size=args.batch_size, data_root=args.data_root, num_workers=1)
 
 algo = {'fgsm': fgsm_gt, 'bim': ifgsm_gt, 'pgd': pgd_gt}
-# attack_algo = algo[args.attack_algo]
 
 attack_algo = algo[args.attack_algo] if args.attack_algo is not None else None
 defend_algo = algo[args.defend_algo] if args.defend_algo is not None else None
@@ -176,16 +169,13 @@ elif args.lr_special:
         weights_list, ranks_up = model_base.svd_global_lowrank_weights(k=args.prune_ratio)
         print(ranks_up)
         model = CLlr(depth=16, num_classes=10, widen_factor=8, dropRate=0.4, ranks=ranks_up)
-        # print([weight.shape for weight in weights_list])
-        # print(weights_list)
+
         model.set_weights(weights_list)
         model.load_state_dict(model_base.state_dict(), strict=False)
-        # descripter = "{}_proj_{}_nnz_{}_quant_{}_bits_{}_".format(defend_name, prune_name, args.prune_ratio, quantize_name, args.quantize_bits) 
-        # with open(os.path.join(args.savedir, "param_" + descripter + "lr.npy"), "wb") as filestream:
+
         with open(os.path.join(args.savedir, args.save_model_name[0:-4] + ".npy"), "wb") as filestream:
             pickle.dump(ranks_up, filestream, protocol=pickle.HIGHEST_PROTOCOL)
     else:
-        # with open(os.path.join(args.loaddir, "param_" + descripter + "lr.npy"), "rb") as filestream:
         with open(os.path.join(args.loaddir, args.save_model_name[0:-4] + ".npy"), "rb") as filestream:
             ranks_up = pickle.load(filestream)
         model = CLlr(depth=16, num_classes=10, widen_factor=8, dropRate=0.4, ranks=ranks_up)
@@ -202,10 +192,6 @@ modelu.empty_all()
 
 modelz.load_state_dict(model.state_dict())
 
-# if not args.raw_train:
-#     modelz.load_state_dict()
-# model_feature = torch.nn.DataParallel(model_feature, device_ids=range(args.ngpu))
-
 if args.cuda:
     model = torch.nn.DataParallel(model, device_ids=range(args.ngpu))
     modelu = torch.nn.DataParallel(modelu, device_ids=range(args.ngpu))
@@ -214,10 +200,8 @@ if args.cuda:
     modelu.cuda()
     modelz.cuda()
 
-# optimizer = optim.Adam(model.parameters(), lr=args.lr)
 optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.wd, momentum=0.9)
-# optim_pred = optim.Adam(list(model_feature.parameters()) + list(model_pred.parameters()), lr=args.lr)
-# optim_distortion = optim.Adam(model_distortion.parameters())
+
 
 decreasing_lr = list(map(int, args.decreasing_lr.split(',')))
 
@@ -239,9 +223,7 @@ try:
 
     layers_n = pt.layers_n(model_base, param_name=["weight"])[1]
     all_num = sum(layers_n.values())
-    # print(all_num)
     sparse_factor = int(all_num * args.prune_ratio)
-    # print(sparse_factor)
     model_size = sparse_factor * args.quantize_bits
     print("\t MODEL SIZE {}".format(model_size))
     weight_bits = [args.quantize_bits for _ in layers] if args.quantize_algo == "model_size_quant" else None
@@ -254,7 +236,6 @@ try:
     print(args.prune_algo)
 
     if args.prune_algo == "baseline":
-        # prune_idx, Weight_shapes = prune_algo(model, args.prune_ratio, param_name=weight_name)
         prune_idx, Weight_shapes = prune_algo(model, sparse_factor, normalized=False, param_name=weight_name)
         prune_lambda = lambda m: idxproj(m, z_idx=prune_idx, W_shapes=Weight_shapes)
     elif args.prune_algo == "l0proj":
@@ -284,7 +265,6 @@ try:
         if epoch in decreasing_lr:
             optimizer.param_groups[0]['lr'] *= 0.1
         
-        # print("quant_interval {}".format(args.quant_interval))
         message = model_train_proj_prune_admm_quant([model, modelz, modelu], epoch, train_loader, optimizer,
             dfn_algo=defend_algo, dfn_eps=args.defend_eps, 
             log_interval=args.log_interval, iscuda=args.cuda, nnzfix=args.prune_algo=="baseline",
@@ -304,13 +284,10 @@ try:
 
         print("Elapsed {:.2f}s, {:.2f} s/epoch, {:.2f} s/batch, ets {:.2f}s".format(
             elapse_time, speed_epoch, speed_batch, eta))
-        
-        # if train_acc > best_train_acc:
-            # descripter = "{}_proj_{}_nnz_{}_quant_{}_bits_{}_".format(defend_name, prune_name, args.prune_ratio, quantize_name, args.quantize_bits)
+
         misc.model_saver(model, args.savedir, args.save_model_name, "sparse_latest_" + descripter)
         if quantize_algo is not None:
             misc.model_saver(modelz, args.savedir, args.save_model_name, "quant_latest_" + descripter)
-            # best_train_acc = train_acc
 
         if epoch % args.test_interval == 0:
 
@@ -318,9 +295,7 @@ try:
                     atk_algo=attack_algo, atk_eps=args.attack_eps, 
                     iscuda=args.cuda, adv_iter=args.defend_iter, criterion=F.cross_entropy)
 
-            # acc = model_uai_test(model_feature, model_pred, epoch, test_loader, attack_algo, args.attack_eps, iscuda=args.cuda, adv_iter=16, criterion=F.cross_entropy)
             layers = layers_nnz(model, param_name=weight_name)[1]
-            # misc.print_dict(layers, name="MODEL SIZE")
             if quantize_lambda is not None:
                 if isinstance(modelz, nn.DataParallel):
                     modelz.module.replace_bias(model.module, weight_name)
@@ -337,30 +312,15 @@ try:
                 print("\t model dist: {}, layer-wise dist :{}, relative: {}".format(dist, dist_layer, relative_layer))
                 if dist < best_dist:
                     best_dist = dist
-                    # descripter = "{}_proj_{}_nnz_{}_quant_{}_bits_{}_".format(defend_name, prune_name, args.prune_ratio, quantize_name, args.quantize_bits)
                     misc.model_saver(model, args.savedir, args.save_model_name, "sparse_closest_" + descripter)
                     misc.model_saver(modelz, args.savedir, args.save_model_name, "quant_closest_" + descripter)
                     print("\t\t closest model saved")
                 
             if acc_adv > best_acc:
-                # new_file = os.path.join(args.loaddir, 'best-{}.pth'.format(epoch))
-                # descripter = args.defend_algo if args.defend_algo is not None else ""
-                # descripter = "{}_proj_{}_nnz_{}_quant_{}_bits_{}_".format(defend_name, prune_name, args.prune_ratio, quantize_name, args.quantize_bits)
                 misc.model_saver(model, args.savedir, args.save_model_name, "sparse_" + descripter)
                 if quantize_algo is not None:
                     misc.model_saver(modelz, args.savedir, args.save_model_name, "quant_" + descripter)
-                # if args.save_model_name is None:
-                #     if args.defend_algo is not None:
-                #         misc.model_snapshot(model, os.path.join(args.savedir, "sparse_" + args.defend_algo+'_densepretrain.pth'))
-                #         misc.model_snapshot(modelz, os.path.join(args.savedir, "quant_" + args.defend_algo + "_densepretrain.pth"))
-                #     else:
-                #         misc.model_snapshot(model, os.path.join(args.savedir, 'sparse_densepretrain.pth'))
-                #         misc.model_snapshot(modelz, os.path.join(args.savedir, 'quant_densepretrain.pth'))
-                # else:
-                #     misc.model_snapshot(model, os.path.join(args.savedir, "sparse_" + args.save_model_name))
-                #     misc.model_snapshot(modelz, os.path.join(args.savedir, "quant_" + args.save_model_name))
                 best_acc = acc_adv
-                # old_file = new_file
 
 except Exception as e:
     import traceback
